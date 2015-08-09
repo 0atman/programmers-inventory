a11a08d64e6b1cffc2452ee4946ffe87daf4cf7c
Tristram Oaten <tristram@oaten.name>
2015-08-09 16:46:59 +0100

    v1, all data pulled from github

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, namedtuple
 
 from django.shortcuts import render
 from requests import get
@@ -13,6 +13,34 @@ def dashboard(request, username):
     github_user = github.users.get(username)
     "https://api.stackexchange.com/2.2/users/333294?site=stackoverflow"
     repos =  github.users(username).repos.get()
-    skills = Counter([repo['language'] for repo in repos])
+    skills_count = dict(Counter([repo['language'] for repo in repos]))
+    Skill = namedtuple('Skill', "name level font_name")
 
+    if skills_count.get(None):
+        del skills_count[None]
+
+    icon_replacements ={
+        "CSS": "css3",
+        "Shell": "linux",
+        "Makefile": "linux",
+        "HTML": "html5",
+        "Puppet": "ruby",
+        "Scala": "java",
+        "VimL": "linux",
+        "Haskell": "linux",
+        "Clojure": "linux",
+        "Elixir": "linux"
+    }
+    unsorted_skills = [
+        Skill(name=skill[0], level=skill[1], font_name=icon_replacements.get(skill[0], skill[0]))
+        for skill
+        in skills_count.items()
+    ]
+    skills = sorted(unsorted_skills, key=lambda s: s.level, reverse=True)
+    if len(skills) > 8:
+        skills = skills[:8]
+    elif len(skills) > 4:
+        skills = skills[:4]
+
+    gists = github.users(username).gists.get()
     return render(request, 'index.html', locals())
